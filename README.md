ead1367b9f06505ea75b554884c399e972aff1d1
Ryan Ng <ryanng3001@gmail.com>
2023-10-02 23:33:34 -0700

    [2/10] Update to cleaning script

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Worklist and Updates 
 
+## My worklist 
+- [ ] occ_issues (under rgbif) is deprecated - any alternatives?
+- [ ] (for the future) Code that allows cleaning for multiple species?
+- [ ] Explore eddmaps as an extra data source!
+
+## Current progress (as of 2/10)
+- Obtained maps and histograms for 5 plant species in brainstorming doc 
+- Addressed the issue of CoordinateCleaner being retired !!! (downloaded another version from GitHub; 3.0)
+- Note that the new version of CoordinateCleaner no longer require renaming column to decimallongitude and decimallatitude (will give error otherwise!!!)
+- Only issue now is occ_issues deprecation... but not an urgent concern 
+
 ## Current progress (as of 26/9) 
 - Deciphered Hazel's script (adapted from Emma's paper)
 - Found some packages that were installed but not used (I think) so I removed them
 - Fixed minor stuff in the code caused by R updates 
 - Replicated map and histogram for Eurybia conspicua (low occurrence) 
 
-
-## My worklist 
-- [ ] Determine if the 'redundant' libraries are actually redundant or not (think it has something to do with the map visualization)
-- [ ] What to do w/ a number of libraries that are retiring Oct 2023?
-- [ ] occ_issues (under rgbif) is deprecated - any alternatives?
-- [ ] Make a table for plant species, # initial, # after cleaning, time, ...
-- [ ] (for the future) Code that allows cleaning for multiple species?
-- [ ] Explore eddmaps as an extra data source! 
--- r-script/annotated-cleaning-script-r.R
+++ r-script/annotated-cleaning-script-r.R
@@ -1,12 +1,11 @@
 ### STEP 00: install packages ###
 install.packages("devtools")
-install.packages("CoordinateCleaner")
 install.packages("countrycode")
 install.packages("rnaturalearthdata")
 install.packages("maps")
 install.packages("usethis")
 devtools::install_github("ropenscilabs/scrubr")
-
+install_github("ropensci/CoordinateCleaner")
 
 ### STEP 01: load packages ###
 library(rgbif)
@@ -18,10 +17,11 @@ library(CoordinateCleaner) # retire oct 2023
 library(ggplot2)
 library(usethis)
 library(devtools)
+library(sf)
 
 
 ### STEP 03: download data from GBIF ###
-myspecies <- c("Eurybia conspicua") # specify species here !!!
+myspecies <- c("Lonicera dioica") # specify species here !!!
 species_search <- occ_search(scientificName = myspecies, 
                              hasCoordinate = TRUE, 
                              hasGeospatialIssue = FALSE, 
@@ -30,6 +30,7 @@ species_search <- occ_search(scientificName = myspecies,
 
 ### STEP 04: ensure all records have been downloaded (True) ###
 species_search$meta$endOfRecords  
+nrow(species_search$data) 
 
 ### STEP 05: remove records with specific issues ###
 # to see abbreviations, type 'gbif_issues()' 
@@ -76,37 +77,38 @@ species_data$countryCode <-  countrycode(species_data$countryCode,
                                          destination = 'iso3c') 
 
 ### STEP 10: identify and remove flagged records ###
-names(species_data)[2:3] <- c("decimallongitude", "decimallatitude")
 species_data_clean <- species_data%>%
   cc_val()%>% # invalid lat/lon coordinates
   cc_equ()%>% # identical lat/lon
   cc_cap()%>% # coordinates in vicinity of country capitals
   cc_cen()%>% # coordinates in vicinity of country or province centroids
-  cc_coun(iso3 = "countryCode")%>% #c oordinates outside reported country
+  cc_coun(iso3 = "countryCode")%>% #coordinates outside reported country
   cc_gbif()%>% # coordinates assigned to GBIF headquarters
-  cc_inst()%>% # coordinates in the vicinity of biodiversity institutions 
-  cc_zero()%>% # coordinates that are zero 
-  cc_dupl() # duplicate records
+  cc_inst()%>% # coordinates in the vicinity of biodiversity institutions
+  cc_zero()%>% # coordinates that are zero
+  cc_dupl()%>% # duplicate records
+  cc_sea()%>%
+  cc_outl()
 
 ### STEP 11: coordinate uncertainty <1000m ###
 species_data_clean <- coord_uncertain(species_data_clean, 
                                       coorduncertainityLimit = 1000)
 nrow(species_data_clean) # shows the number of records left
 
-### STEP 12A: Map ###  
+### STEP 12A: Histogram frequency ### 
+hist(species_data_clean$year, breaks = 50, 
+     ylab="Frequency", xlab="Year", 
+     main=substitute(paste(italic("Lonicera dioica")," observations by year")),
+     xlim=c(1980, 2023))
+
+### STEP 12B: Map ###  
 wm <- borders("world", colour="gray70", fill="gray70")
 ggplot()+ coord_fixed()+ wm +
   geom_point(data = species_data_clean, 
-             aes(x = decimallongitude, y = decimallatitude),
+             aes(x = decimalLongitude, y = decimalLatitude),
              colour = "black", bg = "olivedrab2", pch = 21, size = 0.75) +
   labs (x = "Longitude", 
         y = "Latitude",
         title = myspecies) + 
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5, face="italic"))
-
-### STEP 12B: Histogram frequency ### 
-hist(species_data_clean$year, breaks = 50, 
-     ylab="Frequency", xlab="Year", 
-     main=substitute(paste(italic("Galeopsis bifida")," observations by year")),
-     xlim=c(1980, 2023))
